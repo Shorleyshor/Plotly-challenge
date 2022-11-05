f5dc379a0be7ccc14b599ed2f2cf91dbff84bd18
shola Daropale <sholadennis@yahoo.com>
2022-11-05 11:28:18 +0100

    added the code to populate the the drop down

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -18,3 +18,20 @@ function init(){
     });
 }
 
+function updateMetadata(sample) {
+    d3.json("./samples.json").then((data) => {
+        var metadata = data.metadata;
+
+        console.log(data);
+
+        var resultArray = metadata.filter(sampleObject => sampleObject.id == sample);
+        var result = resultArray[0];
+        var PANEL = d3.select("#sample-metadata");
+            PANEL.html("");
+
+            //  this populates the lower part of the drop down
+        Object.entries(result).forEach(([key, value]) => {
+            PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`)
+        })
+
+  
